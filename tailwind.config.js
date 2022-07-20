6dedf0434f9987b104e9e99104af5cf4af28b292
shoebabedin <shoeb.abedin@intelli.global>
2022-07-20 12:44:01 +0600

    hotel fainal version

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // important: true,
+  content: ["*", "./pages/*html"],
+  theme: {
+    screens: {
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "16px",
+        sm: "16px",
+        lg: "24px",
+        xl: "40px",
+        "2xl": "60px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+      strategy: "class", // only generate classes
+    }),
+  ],
+};
